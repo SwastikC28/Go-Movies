0f694fc7d5ac519c0de1569dc6a1add09546ff80
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-17 16:15:13 +0530

    Created CRUD Services

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -1,7 +1,11 @@
 package service
 
 import (
+	"fmt"
+	"log"
 	"shared/datastore"
+	"shared/datastore/relationaldb"
+	"user-service/internal/model"
 
 	"github.com/jinzhu/gorm"
 )
@@ -18,6 +22,65 @@ func NewUserService(db *gorm.DB, repo datastore.Repository) *UserService {
 	}
 }
 
-func (service *UserService) Create(out interface{}) error {
+func (service *UserService) Create(newUser *model.User) error {
+	//  Creating unit of work.
+	uow := relationaldb.NewUnitOfWork(service.db, false)
+
+	defer uow.Rollback()
+
+	// Add newUser.
+	err := service.repo.Add(uow, newUser)
+	if err != nil {
+		uow.Rollback()
+		return err
+	}
+
+	uow.Commit()
+	return nil
+}
+
+func (service *UserService) GetAllUsers(users *[]model.User) error {
+	uow := relationaldb.NewUnitOfWork(service.db, true)
+
+	defer uow.Rollback()
+
+	err := service.repo.GetAllRecords(uow, users, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	uow.Commit()
+	return nil
+}
+
+func (service *UserService) GetUser(user *model.User, queryProcessor []datastore.QueryProcessor) error {
+	uow := relationaldb.NewUnitOfWork(service.db, true)
+
+	defer uow.Rollback()
+
+	err := service.repo.GetFirst(uow, &user, queryProcessor)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	uow.Commit()
+	return nil
+}
+
+func (service *UserService) DeleteUser(id string) error {
+	uow := relationaldb.NewUnitOfWork(service.db, true)
+
+	condition := fmt.Sprintf("ID = %s", id)
+
+	defer uow.Rollback()
+	err := service.repo.Delete(uow, &model.User{}, condition)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	uow.Commit()
 	return nil
 }
