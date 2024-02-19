c70ba6eb82e9202651b06b54bb0331aa30957939
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-19 05:34:28 +0530

    Added Timer for connecting to the database

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+var counts int64
+
 func main() {
 	db := connectDB()
 	defer db.Close()
@@ -40,26 +42,40 @@ func main() {
 }
 
 func connectDB() *gorm.DB {
-	url := "root:admin@tcp(localhost:3306)/go-movies?charset=utf8mb4&parseTime=true"
+	// url := "root:admin@tcp(localhost:3306)/go-movies?charset=utf8mb4&parseTime=true"
+	url := "admin:admin@tcp(mariadb:3306)/go-movies?charset=utf8mb4&parseTime=True&loc=Local"
 
-	db, err := gorm.Open("mysql", url)
-	if err != nil {
-		log.Println(err)
-		return nil
+	for {
+		db, err := gorm.Open("mysql", url)
+
+		if err != nil {
+			log.Println("SQLDB not yet ready ...")
+			counts++
+		} else {
+			sqlDB := db.DB()
+			sqlDB.SetMaxIdleConns(500)
+			sqlDB.SetMaxOpenConns(2)
+			sqlDB.SetConnMaxLifetime(3 * time.Minute)
+
+			db.LogMode(true)
+			// utf8_general_ci is the default collate for utf8 and it is okay to not specify it.
+			// ci means case insensitive.
+			// db = db.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
+
+			db.BlockGlobalUpdate(true)
+
+			log.Println("Connected to DB successfully.")
+			return db
+		}
+
+		if counts > 10 {
+			log.Println(err)
+			return nil
+		}
+
+		log.Println("Backing off for 2 seconds")
+		time.Sleep(2 * time.Second)
+		continue
 	}
 
-	sqlDB := db.DB()
-	sqlDB.SetMaxIdleConns(500)
-	sqlDB.SetMaxOpenConns(2)
-	sqlDB.SetConnMaxLifetime(3 * time.Minute)
-
-	db.LogMode(true)
-	// utf8_general_ci is the default collate for utf8 and it is okay to not specify it.
-	// ci means case insensitive.
-	// db = db.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci")
-
-	db.BlockGlobalUpdate(true)
-
-	log.Println("Connected to DB Successfully.")
-	return db
 }
