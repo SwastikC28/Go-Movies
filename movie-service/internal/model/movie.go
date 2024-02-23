e19193ba48629902a9057a787733dea28c7a1309
Swastik <swastikkumar50@gmail.com>
2024-02-23 23:32:47 +0530

    Fixed syntax errors in movie model

--- movie-service/internal/model/movie.go
+++ movie-service/internal/model/movie.go
@@ -9,9 +9,9 @@ import (
 type Movie struct {
 	gorm.Model
 	Title           string     `json:"name" gorm:"type:varchar(100)"`
-	Genre           bool       `json:"genre" gorm:"type:varchar(10)"`
+	Genre           string     `json:"genre" gorm:"type:varchar(10)"`
 	Release_Date    *time.Time `json:"releaseDate"`
 	Director        string     `json:"director" gorm:"type:varchar(20);"`
 	Description     string     `json:"description" gorm:"type:varchar(100);"`
-	Inventory_Count uint       `json:"inventoryCount" gorm:"type:integer,default:0;"`
+	Inventory_Count uint       `json:"inventoryCount" gorm:"type:integer;default:0;"`
 }
