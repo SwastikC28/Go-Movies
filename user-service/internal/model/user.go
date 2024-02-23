1bff7f32475493ff2ac9f6f369863216ce98fd9e
Swastik <swastikkumar50@gmail.com>
2024-02-23 23:33:35 +0530

    Added unique in email field

--- user-service/internal/model/user.go
+++ user-service/internal/model/user.go
@@ -6,6 +6,6 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name" gorm:"type:varchar(100)"`
 	IsAdmin  bool   `json:"isAdmin" gorm:"type:boolean;default:false"`
-	Email    string `json:"email" gorm:"type:varchar(100)"`
+	Email    string `json:"email" gorm:"type:varchar(100);unique"`
 	Password string `json:"password" gorm:"type:varchar(100);"`
 }
