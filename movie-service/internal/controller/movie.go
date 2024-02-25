7b22602dcf3dc837330229f73ede85e939e9042e
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-26 00:13:32 +0530

    Added authentication in create movie api

--- movie-service/internal/controller/movie.go
+++ movie-service/internal/controller/movie.go
@@ -8,6 +8,7 @@ import (
 	"shared/datastore"
 	"shared/middleware"
 	"shared/pkg/web"
+	"shared/security"
 
 	"github.com/gorilla/mux"
 )
@@ -22,10 +23,10 @@ func (controller *MovieController) RegisterRoutes(router *mux.Router) {
 
 	movieRouter.Use(middleware.ReqLogger)
 
-	movieRouter.HandleFunc("", controller.createMovie).Methods(http.MethodPost)
+	movieRouter.HandleFunc("", web.AccessGuard(controller.createMovie)).Methods(http.MethodPost)
 	movieRouter.HandleFunc("", controller.getMovies).Methods(http.MethodGet)
 	movieRouter.HandleFunc("/{id}", controller.getMovieById).Methods(http.MethodGet)
-	movieRouter.HandleFunc("/{id}", controller.deleteMovieById).Methods(http.MethodDelete)
+	movieRouter.HandleFunc("/{id}", web.AccessGuard(controller.deleteMovieById)).Methods(http.MethodDelete)
 }
 
 func NewMovieController(service *service.MovieService) *MovieController {
@@ -37,6 +38,16 @@ func NewMovieController(service *service.MovieService) *MovieController {
 func (controller *MovieController) createMovie(w http.ResponseWriter, r *http.Request) {
 	var movie model.Movie
 	web.UnmarshalJSON(r, &movie)
+
+	token := security.TokenFromContext(r.Context())
+
+	if !token.IsAdmin {
+		web.RespondJSON(w, http.StatusUnauthorized, "User unauthorized to access this route")
+		return
+	}
+
+	// Assign Created By as the user's id from token
+	movie.CreatedBy = token.ID
 
 	err := controller.service.Create(&movie)
 	if err != nil {
@@ -65,7 +76,6 @@ func (controller *MovieController) getMovieById(w http.ResponseWriter, r *http.R
 	vars := mux.Vars(r)
 
 	id := vars["id"]
-	fmt.Println("ID", id)
 
 	queryProcessor := []datastore.QueryProcessor{}
 	queryProcessor = append(queryProcessor, datastore.Filter("ID = ?", (id)))
