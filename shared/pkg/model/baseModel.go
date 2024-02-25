4a03ed0c8cbd475e1cc39b76b5553fc6893f668d
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-26 00:15:05 +0530

    Typo Errors fixed

--- shared/pkg/model/baseModel.go
+++ shared/pkg/model/baseModel.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Base struct {
-	ID        uuid.UUID `gorm:"type:varchar(36);primary_key"`
+	ID        uuid.UUID `gorm:"type:varchar(36);primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
