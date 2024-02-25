16264951249f2b0cbb03659dc55d1628cb87600c
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-26 00:13:45 +0530

    Movie model changes

--- movie-service/internal/model/movie.go
+++ movie-service/internal/model/movie.go
@@ -3,14 +3,17 @@ package model
 import (
 	"shared/pkg/model"
 	"time"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 type Movie struct {
 	model.Base
-	Title           string     `json:"name" gorm:"type:varchar(100)"`
-	Genre           string     `json:"genre" gorm:"type:varchar(10)"`
+	Title           string     `json:"title" gorm:"type:varchar(100)"`
+	Genre           string     `json:"genre" gorm:"type:varchar(30)"`
 	Release_Date    *time.Time `json:"releaseDate"`
-	Director        string     `json:"director" gorm:"type:varchar(20);"`
+	Director        string     `json:"director" gorm:"type:varchar(50);"`
 	Description     string     `json:"description" gorm:"type:varchar(100);"`
 	Inventory_Count uint       `json:"inventoryCount" gorm:"type:integer;default:0;"`
+	CreatedBy       uuid.UUID  `json:"createdBy" gorm:"type:varchar(36);foreignKey"`
 }
