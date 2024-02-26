7eefb3f9e5b94a24de28ab41daa14c1e7746c120
Swastik Chaudhary <swastikkumar50@gmail.com>
2024-02-26 18:20:09 +0530

    Rental Model Added

--- rental-service/internal/model/rental.go
+++ rental-service/internal/model/rental.go
@@ -1,7 +1,26 @@
 package model
 
-import "shared/pkg/model"
+import (
+	"shared/pkg/model"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+type AvailabilityStatus string
+
+const (
+	Paid   AvailabilityStatus = "paid"
+	Unpaid AvailabilityStatus = "unpaid"
+)
 
 type Rental struct {
 	model.Base
+	RentalDate time.Time
+	DueDate    time.Time          `json:"dueDate"`
+	ReturnDate *time.Time         `json:"returnDate"`
+	Status     AvailabilityStatus `json:"status" gorm:"default:'unpaid'"`
+	LateFee    uint               `json:"lateFee"`
+	MovieId    uuid.UUID          `json:"movieId" gorm:"type:varchar(36);foreignKey"`
+	UserId     uuid.UUID          `json:"userId" gorm:"type:varchar(36);foreignKey"`
 }
